fc26a58053cacdad5da6bcdb2c0a0f8847f566b4
BenAAndrew <bandrew01@qub.ac.uk>
2021-06-08 16:29:24 +0100

    Improve FPS counter accuracy

--- python/main.py
+++ python/main.py
@@ -11,7 +11,7 @@ faceCascade = cv2.CascadeClassifier(args.haarcascade_path)
 capture = init_capture_method(args.capture_method)
 min_face_size, half_width, half_height = get_dimensions(capture.get_frame(), args.min_face_scale)
 frames = 0
-start_time = time.time()
+last_frame = time.time()
 if args.arduino_port:
 	arduino = serial.Serial(port=args.arduino_port, baudrate=args.arduino_baudrate, timeout=.1)
 
@@ -19,17 +19,18 @@ while True:
     frame = capture.get_frame()
     face = get_face_position(faceCascade, frame, min_face_size, half_width, half_height)
 
+    if args.arduino_port:
+        send_position_to_arduino(arduino, face, half_width, half_height)
+
     if args.preview:
         show_window(
             frame,
             face=face if args.draw_box else None,
-            fps=int(frames / (time.time() - start_time)) if args.fps else None,
+            fps=int(1 / (time.time() - last_frame)) if args.fps else None,
         )
 
-    if args.arduino_port:
-        send_position_to_arduino(arduino, face, half_width, half_height)
-
     frames += 1
+    last_frame = time.time()
     c = cv2.waitKey(1)
     # ESC key
     if c == 27:
