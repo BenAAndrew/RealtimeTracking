a6ab7a0bb78ed770bf1da89cf527a880434c2a6f
BenAAndrew <bandrew01@qub.ac.uk>
2021-06-08 16:35:54 +0100

    Smooth FPS estimate

--- python/main.py
+++ python/main.py
@@ -5,15 +5,20 @@ import serial
 from capture_methods import init_capture_method
 from utils import get_arguments, get_dimensions, get_face_position, show_window, send_position_to_arduino
 
+
+FPS_SAMPLE = 10
+
 # Setup
 args = get_arguments()
 faceCascade = cv2.CascadeClassifier(args.haarcascade_path)
 capture = init_capture_method(args.capture_method)
 min_face_size, half_width, half_height = get_dimensions(capture.get_frame(), args.min_face_scale)
-frames = 0
-last_frame = time.time()
 if args.arduino_port:
 	arduino = serial.Serial(port=args.arduino_port, baudrate=args.arduino_baudrate, timeout=.1)
+
+frames = 0
+current_fps = 0
+last_sample = time.time()
 
 while True:
     frame = capture.get_frame()
@@ -26,11 +31,15 @@ while True:
         show_window(
             frame,
             face=face if args.draw_box else None,
-            fps=int(1 / (time.time() - last_frame)) if args.fps else None,
+            fps=current_fps if args.fps else None,
         )
 
+    # Update FPS
+    if frames % FPS_SAMPLE == 0:
+        current_fps = int(FPS_SAMPLE / (time.time() - last_sample))
+        last_sample = time.time()
+    
     frames += 1
-    last_frame = time.time()
     c = cv2.waitKey(1)
     # ESC key
     if c == 27:
