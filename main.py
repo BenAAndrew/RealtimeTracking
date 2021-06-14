78110e1dc5e204c613a5850c89088dbed1dfab2f
BenAAndrew <bandrew01@qub.ac.uk>
2021-06-14 19:32:08 +0100

    handle expceptions

--- main.py
+++ main.py
@@ -22,36 +22,37 @@ frames = 0
 current_fps = 0
 last_sample = time.time()
 
-while True:
-    frame = capture.get_frame()
-    face = get_face_position(faceCascade, frame, min_face_size, half_width, half_height)
+try:
+    while True:
+        frame = capture.get_frame()
+        face = get_face_position(faceCascade, frame, min_face_size, half_width, half_height)
+
+        if args.motor_control:
+            send_position_to_motors(face, pan, half_width, half_height)
+
+        if args.preview:
+            show_window(
+                frame,
+                face=face if args.draw_box else None,
+                fps=current_fps if args.fps else None,
+            )
+
+        # Update FPS
+        if frames % FPS_SAMPLE == 0:
+            current_fps = int(FPS_SAMPLE / (time.time() - last_sample))
+            last_sample = time.time()
+
+        frames += 1
+        c = cv2.waitKey(1)
+        # ESC key
+        if c == 27:
+            break
+finally:
+    # Close capture & cv2 window
+    capture.close()
+    cv2.destroyAllWindows()
 
     if args.motor_control:
-        send_position_to_motors(face, pan, half_width, half_height)
-
-    if args.preview:
-        show_window(
-            frame,
-            face=face if args.draw_box else None,
-            fps=current_fps if args.fps else None,
-        )
-
-    # Update FPS
-    if frames % FPS_SAMPLE == 0:
-        current_fps = int(FPS_SAMPLE / (time.time() - last_sample))
-        last_sample = time.time()
-
-    frames += 1
-    c = cv2.waitKey(1)
-    # ESC key
-    if c == 27:
-        break
-
-# Close capture & cv2 window
-capture.close()
-cv2.destroyAllWindows()
-
-if args.motor_control:
-    import RPi.GPIO as GPIO
-    pan.stop()
-    GPIO.cleanup()
+        import RPi.GPIO as GPIO
+        pan.stop()
+        GPIO.cleanup()
