532a2b44394f1f707a8e76e5fbd94e84be2555ef
BenAAndrew <benandrew89@gmail.com>
2021-06-14 21:00:13 +0100

    pigpio motor control

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from utils import get_arguments, get_dimensions, get_face_position, show_window
 
 
 FPS_SAMPLE = 10
-PAN_MOTOR_PIN = 17
-TILT_MOTOR_PIN = 27
 
 # Setup
 args = get_arguments()
@@ -15,8 +13,9 @@ capture = init_capture_method(args.capture_method)
 min_face_size, half_width, half_height = get_dimensions(capture.get_frame(), args.min_face_scale)
 
 if args.motor_control:
-    from motor_control import connect_motor, send_position_to_motors
-    pan = connect_motor(PAN_MOTOR_PIN)
+    from motor_control import send_position_to_motors, PAN_MOTOR_PIN, TILT_MOTOR_PIN
+    import pigpio
+    pi = pigpio.pi()
 
 frames = 0
 current_fps = 0
@@ -28,7 +27,7 @@ try:
         face = get_face_position(faceCascade, frame, min_face_size, half_width, half_height)
 
         if args.motor_control and face is not None:
-            send_position_to_motors(face, pan, half_width, half_height)
+            send_position_to_motors(face, half_width, half_height)
 
         if args.preview:
             show_window(
@@ -53,6 +52,5 @@ finally:
     cv2.destroyAllWindows()
 
     if args.motor_control:
-        import RPi.GPIO as GPIO
-        pan.stop()
-        GPIO.cleanup()
+        pi.set_servo_pulsewidth(PAN_MOTOR_PIN, 0)
+        pi.stop()
--- motor_control.py
+++ motor_control.py
@@ -1,39 +1,38 @@
-import RPi.GPIO as GPIO
 import time
 
-REFRESH_RATE = 50
+PAN_MOTOR_PIN = 17
+TILT_MOTOR_PIN = 27
+MOVEMENT_MULTIPLIER = 5
 SLEEP_TIME = 0.15
-MIN_VALUE = 2.5
-MAX_VALUE = 12.5
+MIN_VALUE = 500
+MAX_VALUE = 2500
 RANGE = MAX_VALUE - MIN_VALUE
 
 
-def connect_motor(pin):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin, GPIO.OUT)
-    p = GPIO.PWM(pin, REFRESH_RATE)
-    p.start(MIN_VALUE)
-    return p
+class Motor:
+    def __init__(self, pin):
+        self.pin = pin
+        self.position = MIN_VALUE
+        
+    def change_position(self, change):
+        new_position = self.position + change
+        if new_position >= MIN_VALUE and new_position <= MAX_VALUE:
+            self.position = new_position
+            pi.set_servo_pulsewidth(self.motor_pin, self.position)
+            time.sleep(SLEEP_TIME)
+        else:
+            print(f"{new_position} outside range")
+            
+    def close(self, pi):
+        pi.set_servo_pulsewidth(self.pin, 0)
 
 
-def change_position(motor, position):
-    if position >= MIN_VALUE and position <= MAX_VALUE:
-        motor.ChangeDutyCycle(position)
-        time.sleep(SLEEP_TIME)
-    else:
-        print(f"{position} outside range")
 
-
-def send_position_to_motors(face, pan, half_width, half_height):
+def send_position_to_motors(face, half_width, half_height):
     x, y, w, h = face
     centre_x = int(x + w / 2)
     # centre_y = int(y + h / 2)
     x_diff = half_width - centre_x
     # y_diff = half_height - centre_y
     print(x_diff)
-
-
-# motor = connect_motor(17)
-# change_position(motor, 12.5)
-# motor.stop()
-# GPIO.cleanup()
+    change_position()
